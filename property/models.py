67df12c5f0d1558194f70010bbcb5c6344fcdc46
Valery Efremov <valeriy131100@gmail.com>
2021-12-22 10:34:05 +0300

    Make field Flat.liked_by blankable

--- property/models.py
+++ property/models.py
@@ -58,7 +58,8 @@ class Flat(models.Model):
     liked_by = models.ManyToManyField(
         User,
         related_name='liked_flats',
-        verbose_name='Кто лайкнул'
+        verbose_name='Кто лайкнул',
+        blank=True
     )
 
     def __str__(self):
