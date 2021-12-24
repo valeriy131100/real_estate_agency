9ed33ea7f247bbdf20509e87756c80e461453e9f
Valery Efremov <valeriy131100@gmail.com>
2021-12-24 11:22:47 +0300

    Add indexes to Flat owner fields

--- property/models.py
+++ property/models.py
@@ -5,11 +5,20 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner = models.CharField(
+        'ФИО владельца',
+        max_length=200,
+        db_index=True
+    )
+    owners_phonenumber = models.CharField(
+        'Номер владельца',
+        max_length=20,
+        db_index=True
+    )
     owner_pure_phone = PhoneNumberField(
         'Нормализованный номер владельца',
-        blank=True
+        blank=True,
+        db_index=True
     )
     created_at = models.DateTimeField(
         'Когда создано объявление',
