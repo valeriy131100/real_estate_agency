845481c9f234cb3ec275910780277e563616af38
Valery Efremov <valeriy131100@gmail.com>
2021-12-24 10:21:06 +0300

    Add db model for owners

--- property/models.py
+++ property/models.py
@@ -80,3 +80,27 @@ class Complaint(models.Model):
         verbose_name='Квартира, на которую пожаловались'
     )
     text = models.TextField('Текст жалобы')
+
+
+class Owner(models.Model):
+    full_name = models.CharField(
+        'ФИО владельца',
+        max_length=200,
+        db_index=True
+    )
+    phone = models.CharField(
+        'Номер владельца',
+        max_length=20,
+        db_index=True
+    )
+    pure_phone = PhoneNumberField(
+        'Нормализованный номер владельца',
+        blank=True,
+        db_index=True
+    )
+    flats = models.ManyToManyField(
+        Flat,
+        related_name='owners',
+        verbose_name='Квартиры в собственности',
+        blank=True
+    )
