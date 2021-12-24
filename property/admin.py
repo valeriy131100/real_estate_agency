9a039c608591f5bcde504792741c6d7ee38fc46e
Valery Efremov <valeriy131100@gmail.com>
2021-12-24 11:37:06 +0300

    Remove owner info fields from Flat

--- property/admin.py
+++ property/admin.py
@@ -11,9 +11,7 @@ class FlatAdmin(admin.ModelAdmin):
         'price',
         'new_building',
         'construction_year',
-        'town',
-        'owners_phonenumber',
-        'owner_pure_phone'
+        'town'
     ]
     list_editable = ['new_building']
     list_filter = [
--- property/models.py
+++ property/models.py
@@ -5,21 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField(
-        'ФИО владельца',
-        max_length=200,
-        db_index=True
-    )
-    owners_phonenumber = models.CharField(
-        'Номер владельца',
-        max_length=20,
-        db_index=True
-    )
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца',
-        blank=True,
-        db_index=True
-    )
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
