c7877032944ecc773c5126c4ce6e7fb96aa07d1b
Valery Efremov <valeriy131100@gmail.com>
2021-12-26 10:17:34 +0300

    Make migration 0004 code more compact

--- property/migrations/0004_auto_20211222_0935.py
+++ property/migrations/0004_auto_20211222_0935.py
@@ -6,11 +6,7 @@ from django.db import migrations
 def fill_new_building_field(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-        else:
-            flat.new_building = False
-
+        flat.new_building = (flat.construction_year >= 2015)
         flat.save()
 
 
